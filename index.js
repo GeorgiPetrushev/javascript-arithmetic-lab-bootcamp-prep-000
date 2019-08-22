48d63baa4d86aabd6977a022d8b85eee831cf903
Georgi Petrushev <figoto660@gmail.com>
2019-08-22 02:05:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ function dec(a)
 }
 
 
-function makeInt(a){ //Parse a as an integer and return that integer.
-  number = parseInt(a, 10);
+function makeInt(a){ number = parseInt(a, 10);
   return number;
 }
 function preserveDecimal(a){ number = parseFloat(a, 10);
